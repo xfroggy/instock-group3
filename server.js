5529c66bff1c03c0222fdcf56abeb8bebec6a384
itsnickrus <nmr9@students.ptcollege.edu>
2021-04-05 17:12:14 -0400

    create express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Express is running on port ${port}`);
+});
