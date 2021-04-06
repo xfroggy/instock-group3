6c6e62d59b96b2d2f2ae7057e8726a5aa476d88a
xfroggy <tc.smitala@gmail.com>
2021-04-06 16:25:25 -0400

    bringing everone onto same page Tuesday

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const app = express();
 const port = 8080;
 const cors = require("cors");
 
+const inventoryRoutes = require("./server/routes/inventories");
+const warehouseRoutes = require("./server/routes/warehouses");
+
 app.use(cors());
 
 app.use(express.json());
 
+app.use("/api/warehouses/", warehouseRoutes);
+
 app.listen(port, () => {
   console.log(`Express is running on port ${port}`);
 });
+
+
---
+++ server/routes/warehouses.js
@@ -0,0 +1,72 @@
+const router = require("express").Router();
+const fs = require("fs");
+const { check, validationResult } = require('express-validator');
+const path = require('path').resolve(__dirname, '../data');
+
+// PUT - FOR EDITING A WAREHOUSE
+
+router.put("/edit/:id",
+    //  [
+    //     !check('warehouseName').isEmpty(),
+    //     !check('address').isEmpty(),
+    //     !check('city').isEmpty(),
+    //     !check('country').isEmpty(),
+    //     !check('contactName').isEmpty(),
+    //     !check('position').isEmpty(),
+    //     check('phone').isMobilePhone(),
+    //     check('email').isEmail()
+
+    // ],
+    (request, response) => {
+        // const errors = validationResult(request);
+        // if (!errors.isEmpty()) {
+        //     return response.status(522).json({ errors: errors.array() })
+        // }
+
+
+        response.status(201).send("Validation passed and here's the id you need to update");
+
+        // // READ THE JSON FILE AND PARSE
+        // fs.readFile(`${path}/warehouses.json`, 'utf8', (error, warehouseData => {
+        //     if (error) {
+        //         console.error(error);
+        //         return;
+        //     }
+        //     warehouseData = JSON.parse(warehouseData);
+
+        //     //FIND WAREHOUSE BY ID
+        //     let currentWarehouse = warehouseData.find((warehouse) => warehouse.ed == request.params.id);
+
+        //     //UPDATE VALUES IN CURRENT WAREHOUSE WITH NEW VALUES
+        //     const currentWarehouse = {
+        //         warehouseName: request.body.warehouseName,
+        //         address: request.body.name,
+        //         city: request.body.city,
+        //         country: request.body.country,
+        //         contactName: request.body.contactName,
+        //         position: request.body.position,
+        //         phone: request.body.phone,
+        //         email: request.body.email
+        //     }
+
+        //     //WRITE UPDATED DATA TO FILE
+        //     warehouseData.stringify(warehouseData);
+
+        //     try {
+        //         fs.writeFile(`${path}/warehouses.json`, warehouseData, (error) => {
+        //             if (error) {
+        //                 console.error(error);
+        //                 return;
+        //             }
+        //             response.status(201).send("warehouse updated");
+        //         });
+        //     } catch (error) {
+        //         response.status(501).json({
+        //             error: error.message,
+        //         })
+        //     }
+        // }))
+    });
+
+module.exports = router;
+
