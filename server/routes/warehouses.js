53be7327a3e781e34055c349f1e050b4049ba4c3
xfroggy <tc.smitala@gmail.com>
2021-04-07 16:29:11 -0400

    Complete back-end api POST for adding new warehouse

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -2,34 +2,98 @@ const router = require("express").Router();
 const fs = require("fs");
 const { check, validationResult } = require("express-validator");
 const path = require("path").resolve(__dirname, "../data");
+const { v4: uuidv4 } = require('uuid');
 
 // get request for a single warehouse
 router.get("/edit/:id", (req, res) => {
-  //   console.log(res);
- // console.log(path+"/warehouses.json");
-  const warehousesData = fs.readFileSync(path+"/warehouses.json", "utf-8");
-  const warehouseArr = JSON.parse(warehousesData);
-  const singleWarehouse = warehouseArr.find(
-    (item) => item.id === req.params.id
-  );
-  res.send(singleWarehouse);
-
+    //   console.log(res);
+    // console.log(path+"/warehouses.json");
+    const warehousesData = fs.readFileSync(path + "/warehouses.json", "utf-8");
+    const warehouseArr = JSON.parse(warehousesData);
+    const singleWarehouse = warehouseArr.find(
+        (item) => item.id === req.params.id
+    );
+    res.send(singleWarehouse);
+});
 
 
 //GET - List all warehouses
 
 router.get("/", (req, res) => {
-  const warehouseList = (filePath) => {
-    let data = fs.readFileSync(filePath);
-    return JSON.parse(data);
-  };
-  let list = warehouseList(`${path}/warehouses.json`);
-  res.send(list);
+    const warehouseList = (filePath) => {
+        let data = fs.readFileSync(filePath);
+        return JSON.parse(data);
+    };
+    let list = warehouseList(`${path}/warehouses.json`);
+    res.send(list);
 
 });
 
-// PUT - FOR EDITING A WAREHOUSE
+// POST - ADD A WAREHOUSE
 
+router.post("/add",
+    [
+        check('warehouseName').not().isEmpty(),
+        check('address').not().isEmpty(),
+        check('city').not().isEmpty(),
+        check('country').not().isEmpty(),
+        check('contactName').not().isEmpty(),
+        check('position').not().isEmpty(),
+        check('phone').isMobilePhone(['en-US']),
+        check('email').isEmail()
+
+    ],
+    (request, response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            return response.status(522).json({ errors: errors.array() })
+        }
+        response.status(201).send(`Validation passed and here's the id you need to update:  ${request.body.warehouseName}, ${request.body.address}, ${request.body.city},${request.body.country},${request.body.contactName}, ${request.body.position}, ${request.body.phone}, ${request.body.email}`);
+
+        // READ THE JSON FILE AND PARSE
+        fs.readFile(`${path}/warehouses.json`, 'utf8', ((err, warehouseData) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            warehouseData = JSON.parse(warehouseData);
+
+            //UPDATE VALUES IN CURRENT WAREHOUSE WITH NEW VALUES
+            warehouseData.push(
+                {
+                    id: uuidv4(),
+                    name: request.body.warehouseName,
+                    address: request.body.address,
+                    city: request.body.city,
+                    country: request.body.country,
+                    contact: {
+                        name: request.body.contactName,
+                        position: request.body.position,
+                        phone: request.body.phone,
+                        email: request.body.email
+                    }
+                });
+
+            //WRITE UPDATED DATA TO FILE
+            warehouseData = JSON.stringify(warehouseData);
+
+            try {
+                fs.writeFile(`${path}/warehouses.json`, warehouseData, (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    response.status(201).send("warehouse added");
+                });
+            } catch (error) {
+                response.status(501).json({
+                    error: error.message,
+                })
+            }
+        }))
+    });
+
+// PUT - FOR EDITING A WAREHOUSE
 
 router.put("/edit/:id",
     [
@@ -59,7 +123,7 @@ router.put("/edit/:id",
             warehouseData = JSON.parse(warehouseData);
 
             //FIND WAREHOUSE BY ID
-          
+
             let currentIdIndex = warehouseData.findIndex((obj => obj.id == request.params.id));
 
             console.log(currentIdIndex);
