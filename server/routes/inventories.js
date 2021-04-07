1a34bf8de5f082be3957cf9448cb237a2df35aca
miguel787 <miguelraulcruz@gmail.com>
2021-04-07 15:46:45 -0400

    get req for list of inventory items

--- server/routes/inventories.js
+++ server/routes/inventories.js
@@ -1,8 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const app = express();
+const router = require("express").Router();
+const fs = require("fs");
+const path = require("path").resolve(__dirname, "../data");
 
 router.get("/", (req, res) => {
-  console.log(res);
+  const inventoryList = (filePath) => {
+    let data = fs.readFileSync(filePath);
+    return JSON.parse(data);
+  };
+  let list = inventoryList(`${path}/inventories.json`);
+  res.send(list);
+  console.log(list);
 });
 module.exports = router;
