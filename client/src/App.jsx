f0d5bb54ff9b9d4218d24c3d82f6611daa97d3a9
itsnickrus <nmr9@students.ptcollege.edu>
2021-04-07 14:54:26 -0400

    Warehouse details updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/header/header.jsx";
+import WarehouseDetails from "./components/warehouse/WarehouseDetails";
 
 function App() {
   return (
     <div>
       <Router>
         <Header />
+        <WarehouseDetails />
         <Switch>
           <Route exact path="/" />
           <Route exact path="/warehouses" />
