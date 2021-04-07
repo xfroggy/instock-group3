0b3a04497360c814cabd413ea761dfce1ef30729
miguel787 <miguelraulcruz@gmail.com>
2021-04-07 02:26:13 -0400

    get req for list of warehouses

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -1,77 +1,88 @@
 const router = require("express").Router();
 const fs = require("fs");
-const { check, validationResult } = require('express-validator');
-const path = require('path').resolve(__dirname, '../data');
+const { check, validationResult } = require("express-validator");
+const path = require("path").resolve(__dirname, "../data");
+const warehouse = "./data/warehouses.json";
+
+//GET - List all warehouses
+
+const warehouseList = (filePath) => {
+  let data = fs.readFileSync(filePath);
+  return JSON.parse(data);
+};
 
 router.get("/", (req, res) => {
-    console.log(res);
+  let list = warehouseList(warehouse);
+  console.log(list);
+  res.send(list);
 });
 
 // PUT - FOR EDITING A WAREHOUSE
 
-router.put("/edit/:id",
-    //  [
-    //     !check('warehouseName').isEmpty(),
-    //     !check('address').isEmpty(),
-    //     !check('city').isEmpty(),
-    //     !check('country').isEmpty(),
-    //     !check('contactName').isEmpty(),
-    //     !check('position').isEmpty(),
-    //     check('phone').isMobilePhone(),
-    //     check('email').isEmail()
+router.put(
+  "/edit/:id",
+  //  [
+  //     !check('warehouseName').isEmpty(),
+  //     !check('address').isEmpty(),
+  //     !check('city').isEmpty(),
+  //     !check('country').isEmpty(),
+  //     !check('contactName').isEmpty(),
+  //     !check('position').isEmpty(),
+  //     check('phone').isMobilePhone(),
+  //     check('email').isEmail()
 
-    // ],
-    (request, response) => {
-        // const errors = validationResult(request);
-        // if (!errors.isEmpty()) {
-        //     return response.status(522).json({ errors: errors.array() })
-        // }
+  // ],
+  (request, response) => {
+    // const errors = validationResult(request);
+    // if (!errors.isEmpty()) {
+    //     return response.status(522).json({ errors: errors.array() })
+    // }
 
+    response
+      .status(201)
+      .send("Validation passed and here's the id you need to update");
 
-        response.status(201).send("Validation passed and here's the id you need to update");
+    // // READ THE JSON FILE AND PARSE
+    // fs.readFile(`${path}/warehouses.json`, 'utf8', (error, warehouseData => {
+    //     if (error) {
+    //         console.error(error);
+    //         return;
+    //     }
+    //     warehouseData = JSON.parse(warehouseData);
 
-        // // READ THE JSON FILE AND PARSE
-        // fs.readFile(`${path}/warehouses.json`, 'utf8', (error, warehouseData => {
-        //     if (error) {
-        //         console.error(error);
-        //         return;
-        //     }
-        //     warehouseData = JSON.parse(warehouseData);
+    //     //FIND WAREHOUSE BY ID
+    //     let currentWarehouse = warehouseData.find((warehouse) => warehouse.ed == request.params.id);
 
-        //     //FIND WAREHOUSE BY ID
-        //     let currentWarehouse = warehouseData.find((warehouse) => warehouse.ed == request.params.id);
+    //     //UPDATE VALUES IN CURRENT WAREHOUSE WITH NEW VALUES
+    //     const currentWarehouse = {
+    //         warehouseName: request.body.warehouseName,
+    //         address: request.body.name,
+    //         city: request.body.city,
+    //         country: request.body.country,
+    //         contactName: request.body.contactName,
+    //         position: request.body.position,
+    //         phone: request.body.phone,
+    //         email: request.body.email
+    //     }
 
-        //     //UPDATE VALUES IN CURRENT WAREHOUSE WITH NEW VALUES
-        //     const currentWarehouse = {
-        //         warehouseName: request.body.warehouseName,
-        //         address: request.body.name,
-        //         city: request.body.city,
-        //         country: request.body.country,
-        //         contactName: request.body.contactName,
-        //         position: request.body.position,
-        //         phone: request.body.phone,
-        //         email: request.body.email
-        //     }
+    //     //WRITE UPDATED DATA TO FILE
+    //     warehouseData.stringify(warehouseData);
 
-        //     //WRITE UPDATED DATA TO FILE
-        //     warehouseData.stringify(warehouseData);
-
-        //     try {
-        //         fs.writeFile(`${path}/warehouses.json`, warehouseData, (error) => {
-        //             if (error) {
-        //                 console.error(error);
-        //                 return;
-        //             }
-        //             response.status(201).send("warehouse updated");
-        //         });
-        //     } catch (error) {
-        //         response.status(501).json({
-        //             error: error.message,
-        //         })
-        //     }
-        // }))
-    });
+    //     try {
+    //         fs.writeFile(`${path}/warehouses.json`, warehouseData, (error) => {
+    //             if (error) {
+    //                 console.error(error);
+    //                 return;
+    //             }
+    //             response.status(201).send("warehouse updated");
+    //         });
+    //     } catch (error) {
+    //         response.status(501).json({
+    //             error: error.message,
+    //         })
+    //     }
+    // }))
+  }
+);
 
 module.exports = router;
-
-
