3e49897d58a5fe71675d26a68a6ac42376fdddcb
xfroggy <tc.smitala@gmail.com>
2021-04-06 23:40:33 -0400

    removed commented out code in app.js - client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,17 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
-import WarehousePage from "./pages/WarehousePage";
 
 function App() {
   return (
     <div>
       <Router>
-        {/* <Header /> */}
+        <Header />
         <Switch>
-          {/* <Route exact path="/" /> */}
-          <Route exact path="/" component={WarehousePage} />
-          {/* <Route exact path="/inventory" component={InventoryPage} /> */}
-          {/* <Route path="/warehouse/:id" component={WarehouseDetail} />
-          <Route path="/inventory/:id" component={InventoryDetail} /> */}
+          <Route exact path="/" />
+          <Route exact path="/warehouse" component={WarehousePage} />
+          <Route exact path="/inventory" />
+          <Route path="/warehouse/:id" />
+          <Route path="/inventory/:id" />
         </Switch>
       </Router>
     </div>
