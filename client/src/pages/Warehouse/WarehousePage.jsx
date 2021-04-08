27827cbd3d6d082ff12e04eeb53370ed377e72ad
miguel787 <miguelraulcruz@gmail.com>
2021-04-08 01:48:59 -0400

    got rough layout of styling

---
+++ client/src/pages/Warehouse/WarehousePage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./WarehousePage.scss";
+
+export default function WarehousePage() {
+  return (
+    <>
+      <section className="warehouse__parent">
+        <div className="warehouse__background"></div>
+        <div className="warehouse__firstRow">
+          <div className="warehouse__firstRow--box">
+            <div className="warehouse__container">
+              <h1 className="warehouse__title">Warehouses</h1>
+              <div className="warehouse__searchnButton">
+                <input
+                  type=""
+                  placeholder="search"
+                  className="warehouse__searchBar"
+                ></input>
+                <button>+Add New Warehouse</button>
+              </div>
+              <div className="warehouse__bar">
+                <span className="warehouse__bar--arrow">WAREHOUSE</span>
+                <span className="warehouse__bar--arrow">ADDRESS</span>
+                <span className="warehouse__bar--arrow">CONTACT NAME</span>
+                <span className="warehouse__bar--arrow">
+                  CONTACT INFORMATION
+                </span>
+                <span>ACTION</span>
+              </div>
+              <div className="warehouse__tableRow">
+                <span className="warehouse__name">Santa Monica</span>
+                <span className="warehouse__text">
+                  1125 Stanley street, Montreal,CAN
+                </span>
+                <span className="warehouse__text">Brad MacDonald</span>
+                <span className="warehouse__text">+1(629)555-0129</span>
+                <div className="warehouse__imgBox">
+                  <div className="warehouse__deleteImg"></div>
+                  <div className="warehouse__editImg"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
