abb9da6390c8a598fe98cf6fe767e5b1737ed1c2
ayo <ayoraiford@outlook.com>
2021-04-10 15:50:08 -0400

    added css to component.

--- client/src/components/inventories/Inventories.js
+++ client/src/components/inventories/Inventories.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Inventories.scss";
+import arrow from "../../assets/icons/arrow_back-24px.svg";
+import edit from "../../assets/icons/edit-24px.svg";
 
 const ItemDetail = ({ match }) => {
   const [inventoryItem, setIventoryItem] = useState({});
@@ -17,12 +19,27 @@ const ItemDetail = ({ match }) => {
 
   return (
     <div className="inventory__container">
-      <h1>{inventoryItem.itemName}</h1>
-      <div>{inventoryItem.description}</div>
-      <div>{inventoryItem.category}</div>
-      <div>{inventoryItem.status}</div>
-      <div>{inventoryItem.quantity}</div>
-      <div>{inventoryItem.warehouse} </div>
+      <div className="inventory__top">
+        <img className="arrow" src={arrow} alt="arrow" />
+        <h2>{inventoryItem.itemName}</h2>
+        <img className="edit" src={edit} alt="edit" />
+      </div>
+      <div className="inventory__info">
+        <h6>ITEM DESCRIPTION:</h6>
+        {inventoryItem.description}
+        <h6>CATEGORY:</h6>
+        <div>{inventoryItem.category}</div>
+        <h6>STATUS:</h6>
+        <div className="inventory__details">
+          <div className="inventory__status">{inventoryItem.status}</div>
+          <div className="inventory__quantity">
+            <h6>QUANTITY:</h6>
+            <div>{inventoryItem.quantity}</div>
+          </div>
+        </div>
+        <h6>WAREHOUSE:</h6>
+        <div>{inventoryItem.warehouse}</div>
+      </div>
     </div>
   );
 };
