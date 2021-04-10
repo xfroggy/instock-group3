81b3f4302936d469c9f9d73f52b2121f2cab0606
xfroggy <tc.smitala@gmail.com>
2021-04-10 15:42:41 -0400

    before big github pull

--- client/src/components/warehouse/WarehouseAdd.jsx
+++ client/src/components/warehouse/WarehouseAdd.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import "./WarehouseAdd.scss";
 import { useHistory } from "react-router-dom";
 import axios from 'axios';
 
@@ -120,86 +121,109 @@ const WarehouseAdd = () => {
 
     return (
         <>
-            <div className="card__container">
-                <div className
-                    className="card__header--container">
-                    <div className="card__arrow"></div>
-                    <h1 className="card__header--title">Add Warehouse</h1>
-                </div>
-            </div>
+            {/* <div className="card__container">
+                <div className="form__container">
+                    <div className="card__header--container">
+                        <div className="card__arrow"></div>
+                        <h1 className="card__header--title">Add Warehouse</h1>
+                    </div> */}
 
-            <form onSubmit={addWarehouse}>
-                <div className="card__details--container">
-                    <h2>Warehouse Details</h2>
-                    <div className="form-group">
-                        <div className="form-label">Warehouse Name</div>
-                        <input type="text"
-                            name="warehouseName"
-                        />
-                        <div className="text-danger">
-                            {formContents && !formContents.warehouseName ? warehouseName.error : ""}
-                        </div>
-                    </div>
+            <section className="warehouse__parent">
+                <div className="warehouse__background--gray">
+                    <div className="warehouse__background"></div>
+                    <div className="warehouse__firstRow">
+                        <div className="warehouse__firstRow--box">
+                            <div className="warehouse__container">
+                                <div className="warehouse__box--row">
 
-                    <div className="form-group">
-                        <div className="form-label">Street Address</div>
-                        <input type="text" name="address"
-                        />
-                        <div className="text-danger">
-                            {formContents && !formContents.address ? address.error : ""}
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="form-label">City</div>
-                        <input type="text" name="city" />
-                        <div className="text-danger">
-                            {formContents && !formContents.city ? city.error : ""}
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="form-label">Country</div>
-                        <input type="text" name="country" />
-                        <div className="text-danger">
-                            {formContents && !formContents.country ? country.error : ""}
+                                    <div className="card__top--container">
+                                        <div className="card__arrow"></div>
+                                        <h1 className="card__top--title">Add New Warehouse</h1>
+                                    </div>
+
+
+
+                                    <form onSubmit={addWarehouse}>
+                                        <div className="card__details--container warehouse__tableRow">
+                                            <h2 className="card__details--title">Warehouse Details</h2>
+                                            <div className="form__group">
+                                                <div className="form__label">Warehouse Name</div>
+                                                <input className="form__input" type="text"
+                                                    name="warehouseName"
+                                                    placeholder="Warehouse Name"
+                                                />
+                                                <div className="text-danger">
+                                                    {formContents && !formContents.warehouseName ? warehouseName.error : ""}
+                                                </div>
+                                            </div>
+
+                                            <div className="form__group">
+                                                <div className="form__label">Street Address</div>
+                                                <input className="form__input" type="text" name="address"
+                                                    placeholder="Street Address"
+                                                />
+                                                <div className="text-danger">
+                                                    {formContents && !formContents.address ? address.error : ""}
+                                                </div>
+                                            </div>
+                                            <div className="form__group">
+                                                <div className="form__label">City</div>
+                                                <input className="form__input" type="text" name="city" placeholder="City" />
+                                                <div className="text-danger">
+                                                    {formContents && !formContents.city ? city.error : ""}
+                                                </div>
+                                            </div>
+                                            <div className="form__group">
+                                                <div className="form__label">Country</div>
+                                                <input className="form__input" type="text" name="country" placeholder="Country" />
+                                                <div className="text-danger">
+                                                    {formContents && !formContents.country ? country.error : ""}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="card__details--container warehouse__tableRow">
+                                            <h2>Contact Details</h2>
+                                            <div className="form__group">
+                                                <div className="form__label">Contact Name</div>
+                                                <input className="form__input" type="text" name="contactName" placeholder="Contact Name" />
+                                                <div className="text-danger">
+                                                    {formContents && !formContents.contactName ? contactName.error : ""}
+                                                </div>
+                                            </div>
+                                            <div className="form__group">
+                                                <div className="form__label">Position</div>
+                                                <input className="form__input" type="text" name="position" placeholder="Position" />
+                                                <div className="text-danger">
+                                                    {formContents && !formContents.position ? position.error : ""}
+                                                </div>
+                                            </div>
+                                            <div className="form__group">
+                                                <div className="form__label">Phone Number</div>
+                                                <input className="form__input" type="text" name="phone" placeholder="Phone Number" />
+                                                <div className="text-danger">
+                                                    {formContents && !formContents.phone ? phone.error : ""}
+                                                </div>
+                                            </div>
+                                            <div className="form__group">
+                                                <div className="form__label">Email</div>
+                                                <input className="form__input" type="text" name="email" placeholder="Email" />
+                                                <div className="text-danger">
+                                                    {formContents && !formContents.email ? email.error : ""}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="form__button-group">
+                                            <button onClick={goToPreviousPath} className="btn-clear">Cancel</button>
+                                            <button className="btn-add" type="submit">+ Add Warehouse</button>
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="card__details--container">
-                    <h2>Contact Details</h2>
-                    <div className="form-group">
-                        <div className="form-label">Contact Name</div>
-                        <input type="text" name="contactName" />
-                        <div className="text-danger">
-                            {formContents && !formContents.contactName ? contactName.error : ""}
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="form-label">Position</div>
-                        <input type="text" name="position" />
-                        <div className="text-danger">
-                            {formContents && !formContents.position ? position.error : ""}
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="form-label">Phone Number</div>
-                        <input type="text" name="phone" placeholder="(xxx) xxx-xxxx" />
-                        <div className="text-danger">
-                            {formContents && !formContents.phone ? phone.error : ""}
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="form-label">Email</div>
-                        <input type="text" name="email" />
-                        <div className="text-danger">
-                            {formContents && !formContents.email ? email.error : ""}
-                        </div>
-                    </div>
-                </div>
-                <div className="form__button-group">
-                    <button onClick={goToPreviousPath} className="btn">Cancel</button>
-                    <button className="btn btn-primary" type="submit">+ Add Warehouse</button>
-                </div>
-            </form>
+            </section>
         </>
     )
 }
