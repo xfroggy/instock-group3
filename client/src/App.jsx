41d88681e1b0fea9e7a19fa2f16a343e3a134fc5
xfroggy <tc.smitala@gmail.com>
2021-04-10 08:05:19 -0400

    complete footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/header/header.jsx";
+import Footer from "./components/Footer/Footer";
 import WarehouseDetails from "./components/warehouse/WarehouseDetails";
 import WarehousePage from "./pages/Warehouse/WarehousePage";
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/warehouses/:id" component={WarehouseDetails} />
           <Route path="/inventory/:id" />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
