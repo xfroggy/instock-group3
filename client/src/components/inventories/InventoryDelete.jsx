64c29c207725f3af7e371d97acac6aa4b64fe815
xfroggy <tc.smitala@gmail.com>
2021-04-11 19:13:26 -0400

    complete inventory delete modal, still working on delete api call

--- client/src/components/inventories/InventoryDelete.jsx
+++ client/src/components/inventories/InventoryDelete.jsx
@@ -2,27 +2,26 @@ import React from 'react'
 import { Modal, Button } from 'react-bootstrap';
 import "./InventoryDelete.scss";
 
+
 const DeleteConfirmation = ({ showModal, hideModal, confirmModal, id, item, type, message }) => {
 
     return (
 
-        <Modal className="modal-custom" show={showModal} onHide={hideModal}>
-            <Modal.Header closeButton>
-                <Modal.Title>Delete {item} inventory item?</Modal.Title>
-            </Modal.Header>
+
+        <Modal show={showModal} onHide={hideModal}>
+            <Modal.Header closeButton></Modal.Header>
+            <Modal.Title>Delete {item} inventory item?</Modal.Title>
+
             <Modal.Body><div className="alert alert-danger">{message}</div></Modal.Body>
             <Modal.Footer>
-                <Button variant="default" onClick={hideModal}>
+                <Button variant="default"
+                    className="rounded" onClick={hideModal}>
                     Cancel
                 </Button>
                 <Button variant="danger" onClick={() => confirmModal(type, id)}>
                     Delete
                 </Button>
             </Modal.Footer>
-            <div class="modal-overlay">
-            </div>
-
-
         </Modal>
 
 
